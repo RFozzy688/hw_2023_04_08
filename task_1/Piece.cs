b39eae464dc2bcb0ae63228c680d3548775f7ab4
RFozzy688 <pokerfozzy@gmail.com>
2023-04-18 15:09:08 +0300

    реализация класса Piece

---
+++ task_1/Piece.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    internal class Piece
+    {
+        string NamePiece { get; set; }
+        string Author { get; set; }
+        string Genre { get; set; }
+        int Year { get; set; }
+
+        public Piece(string namePiece, string author, string genre, int year)
+        {
+            NamePiece = namePiece;
+            Author = author;
+            Genre = genre;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"Название: {NamePiece}\nАвтор: {Author}\nЖанр: {Genre}\nГод: {Year}";
+        }
+    }
+}
