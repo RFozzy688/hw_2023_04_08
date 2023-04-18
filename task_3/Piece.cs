daaf5a638c42b7a88dfde816b2d23643aaacab44
RFozzy688 <pokerfozzy@gmail.com>
2023-04-18 17:26:57 +0300

    добавлены классы Piece, Shop

---
+++ task_3/Piece.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    internal class Piece
+    {
+        public string NamePiece { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+
+        public Piece(string namePiece, string author, string genre, int year)
+        {
+            NamePiece = namePiece;
+            Author = author;
+            Genre = genre;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"Название: {NamePiece}\nАвтор: {Author}\nЖанр: {Genre}\nГод: {Year}";
+        }
+        ~Piece()
+        {
+            Console.WriteLine("Отработал финализатор");
+        }
+    }
+}
